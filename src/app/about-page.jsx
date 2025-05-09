4e43a41dd396977465996ca173a52810b453bb2d
大胃王 <253498229@qq.com>
2025-05-09 13:47:29 +0800

    使用jsx编写标签

---
+++ src/app/about-page.jsx
@@ -0,0 +1,8 @@
+export function AboutPage() {
+  return (
+    <>
+      <h1>关于</h1>
+      <p>你好。<br/>最近怎么样？</p>
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { MyButton } from '@/app/my-button';
+import { AboutPage } from '@/app/about-page';
 
 export default function Home() {
   return (
     <div>
       <h1>Hello world!</h1>
       <MyButton/>
+      <AboutPage/>
     </div>
   );
 }
