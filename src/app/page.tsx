694082370163524c1e44e9b957f7719c371014cf
大胃王 <253498229@qq.com>
2025-05-09 14:53:03 +0800

    渲染列表

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { MyButton } from '@/app/my-button';
 import { AboutPage } from '@/app/about-page';
 import MyProfile from '@/app/my-profile';
+import { ShoppingList } from '@/app/shopping-list';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <MyButton/>
       <AboutPage/>
       <MyProfile/>
+      <ShoppingList/>
     </div>
   );
 }
