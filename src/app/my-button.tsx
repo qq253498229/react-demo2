82ec5336eda0cd57cc9812a4567ca8c3870b18aa
大胃王 <253498229@qq.com>
2025-05-09 13:38:14 +0800

    嵌套组件

---
+++ src/app/my-button.tsx
@@ -0,0 +1,5 @@
+export function MyButton() {
+  return (
+    <button>这是我的button</button>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { MyButton } from '@/app/my-button';
+
 export default function Home() {
   return (
     <div>
       <h1>Hello world!</h1>
+      <MyButton/>
     </div>
   );
 }
