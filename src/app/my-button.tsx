0bdf5f757f5726b19398c2ba276da2ad8f0b5bb3
大胃王 <253498229@qq.com>
2025-05-09 15:15:53 +0800

    更新界面

--- src/app/my-button.tsx
+++ src/app/my-button.tsx
@@ -1,10 +1,16 @@
 'use client';
+
+import { useState } from 'react';
+
 export default function MyButton() {
+  const [clickCount, setClickCount] = useState(0);
+
   function testFun() {
     console.log('testFun');
+    setClickCount(clickCount + 1);
   }
 
   return (
-    <button onClick={testFun}>这是我的button</button>
+    <button onClick={testFun}>这是我的button，已经点击了{clickCount}次</button>
   );
 }
--- src/app/my-profile.tsx
+++ src/app/my-profile.tsx
@@ -1,9 +1,19 @@
+'use client';
+
+import { useState } from 'react';
+
 const user = {
   name: '老王',
   imageUrl: 'https://i.imgur.com/JhLgPTT.jpeg',
   imageSize: 150,
 };
 export default function MyProfile() {
+  const [clickCount, setClickCount] = useState(0);
+
+  function test() {
+    setClickCount(clickCount + 1);
+  }
+
   return (
     <>
       <h1>你好：{user.name}</h1>
@@ -15,6 +25,7 @@ export default function MyProfile() {
           width: user.imageSize,
         }}
       />
+      <button onClick={test}>点击增加count，已经点击了{clickCount}次</button>
     </>
   );
 }
